a08fc05ca6bb9b8917a092ccced6bb39b864f14e
Sander Beijaard <johndoe@example.com>
2021-05-03 10:33:00 +0200

    asd

--- dashboard/dashboard/ClientApp/src/app/app.module.ts
+++ dashboard/dashboard/ClientApp/src/app/app.module.ts
@@ -12,6 +12,13 @@ import { CounterComponent } from './counter/counter.component';
 import { DatetimeComponent } from './dashboard/widgets/datetime/datetime.component';
 import { ClickerComponent } from './dashboard/widgets/clicker/clicker.component';
 import { LoginComponent } from './login/login.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/moment';
+import * as moment from 'moment';
+
+export function momentAdapterFactory() {
+  return adapterFactory(moment);
+};
 
 @NgModule({
   declarations: [
@@ -31,7 +38,8 @@ import { LoginComponent } from './login/login.component';
     RouterModule.forRoot([
       { path: '', component: DashboardComponent, pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
-    ])
+    ]),
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: momentAdapterFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
